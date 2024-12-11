3e03c41cc76718aa77d5a56f9dde33f4e86db97f
Anup50 <kca3734@gmail.com>
2024-12-11 19:43:27 +0545

    configured flutter_native_splash, wip on splash screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,13 @@
-st
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home: SnackbarView(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:work_hive_mobile/app.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       initialRoute: '/',
-      routes: {
-
-      },
+      routes: {},
       home: MyApp(),
     ),
   );
