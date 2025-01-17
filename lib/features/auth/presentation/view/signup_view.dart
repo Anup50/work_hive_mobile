37f1b6a176d0fbea989596ea390b919dba10ec64
Anup50 <kca3734@gmail.com>
2025-01-17 20:57:45 +0545

    implemented bloc in signup

---
+++ lib/features/auth/presentation/view/signup_view.dart
@@ -0,0 +1,280 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:work_hive_mobile/features/auth/presentation/view/signin_view.dart';
+import 'package:work_hive_mobile/features/auth/presentation/view_model/signup/signup_bloc.dart';
+
+class RegisterView extends StatefulWidget {
+  const RegisterView({super.key});
+
+  @override
+  State<RegisterView> createState() => _RegisterViewState();
+}
+
+class _RegisterViewState extends State<RegisterView> {
+  final _gap = const SizedBox(height: 8);
+  final _key = GlobalKey<FormState>();
+  final _nameController = TextEditingController(text: '');
+  final _emailController = TextEditingController(text: '');
+  final _passwordController = TextEditingController(text: '');
+  final _confirmPasswordController = TextEditingController(text: '');
+  String _selectedRole = 'Job Seeker';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/images/Frame 121(2).png',
+                  height: 50,
+                  width: 200,
+                ),
+
+                const SizedBox(height: 16),
+
+                // Title and subtitle
+                const Text(
+                  'Create Your',
+                  style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+                ),
+                const Text(
+                  'Account',
+                  style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 8),
+                const Text('Fill in the details to sign up'),
+                _gap,
+                Form(
+                  key: _key,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  _selectedRole = 'Job Seeker';
+                                });
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: _selectedRole == 'Job Seeker'
+                                    ? Colors.blue
+                                    : Colors.grey[200],
+                                foregroundColor: _selectedRole == 'Job Seeker'
+                                    ? Colors.white
+                                    : Colors.black,
+                              ),
+                              child: const Text('Job Seeker'),
+                            ),
+                          ),
+                          const SizedBox(width: 8),
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: () {
+                                setState(() {
+                                  _selectedRole = 'Employer';
+                                });
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: _selectedRole == 'Employer'
+                                    ? Colors.blue
+                                    : Colors.grey[200],
+                                foregroundColor: _selectedRole == 'Employer'
+                                    ? Colors.white
+                                    : Colors.black,
+                              ),
+                              child: const Text('Employer'),
+                            ),
+                          ),
+                        ],
+                      ),
+                      _gap,
+                      TextFormField(
+                        controller: _nameController,
+                        decoration: InputDecoration(
+                          labelText: 'Full Name',
+                          prefixIcon: const Icon(Icons.person),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: ((value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your name';
+                          }
+                          return null;
+                        }),
+                      ),
+                      _gap,
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          prefixIcon: const Icon(Icons.email),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your email';
+                          }
+
+                          final emailRegex = RegExp(
+                              r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+$');
+                          if (!emailRegex.hasMatch(value)) {
+                            return 'Please enter a valid email address';
+                          }
+
+                          return null;
+                        },
+                      ),
+                      _gap,
+                      TextFormField(
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          prefixIcon: const Icon(Icons.lock),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+
+                          if (value.length < 8) {
+                            return 'Password must be at least 8 characters long';
+                          }
+
+                          final passwordRegex = RegExp(
+                              r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$');
+                          if (!passwordRegex.hasMatch(value)) {
+                            return 'Password must include uppercase, lowercase, number, and special character';
+                          }
+
+                          return null;
+                        },
+                      ),
+                      _gap,
+                      TextFormField(
+                        controller: _confirmPasswordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Confirm Password',
+                          prefixIcon: const Icon(Icons.lock),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please confirm your password';
+                          }
+
+                          if (value != _passwordController.text) {
+                            return 'Passwords do not match';
+                          }
+
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (_key.currentState!.validate()) {
+                              context.read<SignupBloc>().add(
+                                    RegisterUser(
+                                      context: context,
+                                      name: _nameController.text,
+                                      email: _emailController.text,
+                                      password: _passwordController.text,
+                                      role: _selectedRole,
+                                    ),
+                                  );
+                            }
+                          },
+                          child: const Text('Register'),
+                        ),
+                      ),
+
+                      const Center(
+                        child: Text(
+                          'Or',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ),
+
+                      _gap,
+
+                      // Google sign-up button
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          // Add Google sign-up logic
+                        },
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(double.infinity, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          backgroundColor:
+                              const Color.fromARGB(255, 193, 227, 242),
+                          foregroundColor: Colors.blue,
+                        ),
+                        icon: Image.asset(
+                          'assets/icons/icons8-google-48.png',
+                          height: 24,
+                          width: 24,
+                        ),
+                        label: const Text('Sign Up with Google',
+                            style: TextStyle(
+                              fontFamily: 'PlusJakartaSans SemiBold',
+                            )),
+                      ),
+
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 45),
+                            child: Center(
+                              child: TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => SignInPage(),
+                                    ),
+                                  );
+                                },
+                                child: const Text(
+                                    "Already have an account? Sign In"),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
