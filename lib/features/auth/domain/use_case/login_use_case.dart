363486f6ff1d1037d0a81b721f560ecf0f9219a4
Anup50 <kca3734@gmail.com>
2025-01-31 23:33:19 +0545

    modified to save token in shared prefs

--- lib/features/auth/domain/use_case/login_use_case.dart
+++ lib/features/auth/domain/use_case/login_use_case.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:work_hive_mobile/app/shared_prefs/token_shared_prefs.dart';
 import 'package:work_hive_mobile/app/usecase/usecase.dart';
 import 'package:work_hive_mobile/core/error/failure.dart';
 import 'package:work_hive_mobile/features/auth/domain/repository/auth_repository.dart';
@@ -24,11 +25,22 @@ class LoginParams extends Equatable {
 
 class LoginUseCase implements UsecaseWithParams<String, LoginParams> {
   final IAuthRepository repository;
-
-  LoginUseCase(this.repository);
+  final TokenSharedPrefs tokenSharedPrefs;
+  LoginUseCase(this.repository, this.tokenSharedPrefs);
 
   @override
   Future<Either<Failure, String>> call(LoginParams params) {
-    return repository.loginUser(params.email, params.password);
+    return repository.loginUser(params.email, params.password).then((value) {
+      return value.fold(
+        (failure) => Left(failure),
+        (token) {
+          tokenSharedPrefs.saveToken(token);
+          tokenSharedPrefs.getToken().then((value) {
+            print(value);
+          });
+          return Right(token);
+        },
+      );
+    });
   }
 }
