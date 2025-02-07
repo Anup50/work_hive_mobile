dbe3ee959b8010103dc8411826f2bfc51bbe9f7c
Anup50 <kca3734@gmail.com>
2025-02-07 16:57:59 +0545

    created mock rrpo for job seekers repo

---
+++ test/features/user_data/domain/use_case/repository.mock.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:work_hive_mobile/features/user_data/domain/repository/job_seeker_repository.dart';
+
+class MockJobSeekerRepository extends Mock implements IJobSeekerRepository {}
