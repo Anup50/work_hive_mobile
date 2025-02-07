eee762ce2f25ea8c07ee0fd1168e6063a9eaf626
Anup50 <kca3734@gmail.com>
2025-02-07 17:01:25 +0545

    created empty constructor for fallback

--- lib/features/user_data/domain/entity/job_seeker_entity.dart
+++ lib/features/user_data/domain/entity/job_seeker_entity.dart
@@ -19,6 +19,15 @@ class JobSeekerEntity extends Equatable {
     this.updatedAt,
   });
 
+  const JobSeekerEntity.empty()
+      : userId = null,
+        profilePicture = null,
+        bio = null,
+        location = null,
+        skills = const [],
+        createdAt = null,
+        updatedAt = null;
+
   @override
   List<Object?> get props =>
       [userId, profilePicture, bio, location, skills, createdAt, updatedAt];
