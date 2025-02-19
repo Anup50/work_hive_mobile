a2e77cf7ab6299ff3e63255616be01d375d10fb6
Anup50 <kca3734@gmail.com>
2025-02-20 02:24:50 +0545

    fixed import for entity

--- lib/features/jobs/data/data_source/job_data_source.dart
+++ lib/features/jobs/data/data_source/job_data_source.dart
@@ -1,4 +1,4 @@
-import 'package:work_hive_mobile/features/jobs/domain/entity/job_entty.dart';
+import 'package:work_hive_mobile/features/jobs/domain/entity/job_entity.dart';
 
 abstract interface class IJobDataSource {
   Future<List<JobEntity>> getJobs();
