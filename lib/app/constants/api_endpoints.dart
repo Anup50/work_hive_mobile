f0b58817b3c5ceb7022f100debe66e098ce3b65c
Anup50 <kca3734@gmail.com>
2025-03-04 00:50:17 +0545

    endppoint for get recommended

--- lib/app/constants/api_endpoints.dart
+++ lib/app/constants/api_endpoints.dart
@@ -5,8 +5,8 @@ class ApiEndpoints {
   static const Duration receiveTimeout = Duration(seconds: 1000);
   //for emulator
   // static const String baseUrl = "http://10.0.2.2:3000/api/";
-  // static const String baseUrl = "http://192.168.18.11:3000/api/";
-  static const String baseUrl = "http://192.168.1.5:3000/api/";
+  static const String baseUrl = "http://192.168.18.11:3000/api/";
+  // static const String baseUrl = "http://192.168.1.5:3000/api/";
 
   // For iphone
   // static const String baseUrl = "http://localhost:3000/api/v1/";
@@ -23,6 +23,6 @@ class ApiEndpoints {
   static const String createJob = "job/";
   static const String deleteJob = "job/";
   static const String getAllJob = "job/getall";
-  static const String getRecommended = "job/recommended";
+  static const String getRecommended = "job/recommended/";
   static const String getById = "job/";
 }
